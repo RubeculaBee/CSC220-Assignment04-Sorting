8fefa756bd5ae1543ff35df62e70ddea15b13f0f
Robin Lane <rubeculabee@gmail.com>
2025-10-19 14:55:37 -0700

    commented SortingAlgorithm.java

--- src/assignment04PartA/SortingAlgorithm.java
+++ src/assignment04PartA/SortingAlgorithm.java
@@ -1,18 +1,43 @@
 package assignment04PartA;
 
+/**
+ * * File: SortingAlgorithm.java
+ * * By:   Robin Lane
+ * * Date: 10-19-25
+ * 
+ * * Description: 3 Sorting algorithms (Selection, Insertion, and Shell sort)
+ * *              used for Part A of this assignment. Designed to only work with
+ * *              ints, as that is all that was requested by the client.
+ */
+
+
+ /**
+  * * Super class that all sorting algorithms will extend, ensuring they have a sort method, can swap indices, and can display contents of an array
+  */
 sealed abstract class SortingAlgorithm
 {
+    /**
+     * {@param array An integer array to be sorted}
+     */
     abstract void sort(int[] array);
 
+    /**
+     * {@param array The array whose contents will be displayed}
+     */
     static void displayArray(int[] array)
     {
         System.out.print("{");
         for(int n : array)
             System.out.printf("%s, ", n);
+        // The two backspaces remove the final comma and space, since they would follow the last entry
         System.out.print("\b\b}\n");
     }
 
-    // Swaps the data in indexes i and j of the given array
+    /**
+     * {@param array The array to swap items in}
+     * {@param i The index of the first item that will be swapped with index j}
+     * {@param j The index of the second item that will be swapped with index i}
+     */
     void swap(int[] array, int i, int j)
     {
         int temp = array[i];
@@ -23,6 +48,9 @@ sealed abstract class SortingAlgorithm
     }
 }
 
+/**
+ * * Searches the unsorted array for the smallest number, and swaps it with the first index of the unsorted array
+ */
 final class SelectionSort extends SortingAlgorithm
 {
     public void sort(int[] array)
@@ -47,6 +75,21 @@ final class SelectionSort extends SortingAlgorithm
     }
 }
 
+/**
+ * * For each index in the array, move the item back until it's larger than everything before it but smaller than anything after it
+ */
+final class InsertionSort extends SortingAlgorithm
+{
+    public void sort(int[] array)
+    {
+        // TODO: Implement Sorting Algorithm
+    }
+}
+
+/**
+ * * A modification of insertion sort. Sort (via insertion sort) sub-arrays of the array that are n indices apart. After each pass, n is halved.
+ * * Insertion sort works better on lists that are "almost sorted", thus by sorting several smaller sublists, we can sort a total list faster.
+ */
 final class ShellSort extends SortingAlgorithm
 {
     public void sort(int[] array)
@@ -55,10 +98,4 @@ final class ShellSort extends SortingAlgorithm
     }
 }
 
-final class InsertionSort extends SortingAlgorithm
-{
-    public void sort(int[] array)
-    {
-        // TODO: Implement Sorting Algorithm
-    }
-}
+
