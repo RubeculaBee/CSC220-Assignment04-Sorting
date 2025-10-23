524cb881add6d781d8df10bb91d69dc266042c81
Robin Lane <rubeculabee@gmail.com>
2025-10-22 17:36:58 -0700

    implemented addToBack and addtoFront

--- src/assignment04PartD/CircularDoublyLinkedDeque.java
+++ src/assignment04PartD/CircularDoublyLinkedDeque.java
@@ -11,16 +11,38 @@ package assignment04PartD;
 
 public class CircularDoublyLinkedDeque<T> implements DequeInterface<T>
 {
+    DoublyLinkedNode firstnode;
+
     @Override
     public void addToFront(T newEntry) 
     {
-        // TODO Auto-generated method stub
+        if(firstnode == null)
+        {
+            firstnode = new DoublyLinkedNode(newEntry);
+            return;
+        }
+
+        DoublyLinkedNode currentnode = firstnode;
+        while(currentnode.next != null)
+            currentnode = currentnode.next;
+        
+        currentnode.next = new DoublyLinkedNode(newEntry, currentnode, null);
     }
 
     @Override
     public void addToBack(T newEntry) 
     {
-        // TODO Auto-generated method stub
+        if(firstnode == null)
+        {
+            firstnode = new DoublyLinkedNode(newEntry);
+            return;
+        }
+        
+        DoublyLinkedNode currentnode = firstnode;
+        while(currentnode.prev != null)
+            currentnode = currentnode.next;
+        
+        currentnode.prev = new DoublyLinkedNode(newEntry, null, currentnode);
     }
 
     @Override
