930d2ebec1bbcbe545249d550ef9f67edd8e0d67
Robin Lane <rubeculabee@gmail.com>
2025-10-22 17:11:45 -0700

    Implementd private class DoublyLinkedNode

--- src/assignment04PartD/CircularDoublyLinkedDeque.java
+++ src/assignment04PartD/CircularDoublyLinkedDeque.java
@@ -1,1 +1,25 @@
-package assignment04PartD;public class CircularDoublyLinkedDeque<T> implements DequeInterface<T> {    private class DoublyLinkedNode {            } // end DoublyLinkedNode} // end CircularDoublyLinkedDeque
+package assignment04PartD;
+
+public class CircularDoublyLinkedDeque<T> implements DequeInterface<T>
+{
+
+    private class DoublyLinkedNode 
+    {
+        private T data;
+        private DoublyLinkedNode prev;
+        private DoublyLinkedNode next;
+
+        private DoublyLinkedNode() {}
+
+        private DoublyLinkedNode(T data) {this.data = data;}
+        
+        private DoublyLinkedNode(T data, DoublyLinkedNode prev, DoublyLinkedNode next) 
+        {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+        
+    } // end DoublyLinkedNode
+
+} // end CircularDoublyLinkedDeque
