7ca1841e96d5c3417659926248aaafb0f9320a51
Robin Lane <rubeculabee@gmail.com>
2025-10-22 17:21:59 -0700

    added project header and unimplemented deque methods

--- src/assignment04PartD/CircularDoublyLinkedDeque.java
+++ src/assignment04PartD/CircularDoublyLinkedDeque.java
@@ -1,7 +1,73 @@
 package assignment04PartD;
 
+/*
+ * File: CircularDoublyLinkedDeque.java
+ * By:   Robin Lane
+ * Date: 10-22-25
+ * 
+ * Description: A double ended queue (deque) where the last node references the first node and vice versa
+ *              This way you can iterate backwards and forwards through the deque to reach any item
+ */
+
 public class CircularDoublyLinkedDeque<T> implements DequeInterface<T>
 {
+    @Override
+    public void addToFront(T newEntry) 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void addToBack(T newEntry) 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public T removeFront() 
+    {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public T removeBack() 
+    {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public T getFront() 
+    {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public T getBack() 
+    {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() 
+    {
+        // TODO Auto-generated method stub
+
+        return true;
+    }
+
+    @Override
+    public void clear() 
+    {
+        // TODO Auto-generated method stub
+    }
 
     private class DoublyLinkedNode 
     {
@@ -12,7 +78,7 @@ public class CircularDoublyLinkedDeque<T> implements DequeInterface<T>
         private DoublyLinkedNode() {}
 
         private DoublyLinkedNode(T data) {this.data = data;}
-        
+
         private DoublyLinkedNode(T data, DoublyLinkedNode prev, DoublyLinkedNode next) 
         {
             this.data = data;
