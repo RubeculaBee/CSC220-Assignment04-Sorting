f8a7b7bcb980f1218a932bcdee13decaabdb523d
Robin Lane <rubeculabee@gmail.com>
2025-10-24 14:43:23 -0700

    Implemented student attributes and constructor

--- src/assignment04PartE/Student.java
+++ src/assignment04PartE/Student.java
@@ -6,6 +6,30 @@ package assignment04PartE;
  *
  */
 
-public final class Student implements Comparable<Student> {
+public final class Student implements Comparable<Student>
+{
+    private String firstName;
+    private String lastName;
+    private int id;
+    private double gpa;
+    private int numSmallQuestions;
+    private int numLargeQuestions;
 
+    public Student (String firstName, String lastName, int id, double gpa, int numSmallQuestions, int numLargeQuestions)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.gpa = gpa;
+        this.numSmallQuestions = numSmallQuestions;
+        this.numLargeQuestions =numLargeQuestions;
+    }
+
+    @Override
+    public int compareTo(Student student)
+    {
+        //TODO Implement compareTo
+
+        return 0;
+    }
 }
