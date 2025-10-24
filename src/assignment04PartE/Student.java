d938b82340b8b70ca03aa6a91842978424f268d0
Robin Lane <rubeculabee@gmail.com>
2025-10-24 14:50:59 -0700

    implemented static data fields and methods concerning the setting and getting of sort priority

--- src/assignment04PartE/Student.java
+++ src/assignment04PartE/Student.java
@@ -15,6 +15,19 @@ public final class Student implements Comparable<Student>
     private int numSmallQuestions;
     private int numLargeQuestions;
 
+    static private String currentPriority;
+    static private String[] priorities = 
+    {
+        "realistic", 
+        "first-name", 
+        "last-name", 
+        "student-id", 
+        "gpa", 
+        "number-of-small-questions",
+        "number-of-big-questions",
+        "number-of-small-and-big-questions"
+    };
+
     public Student (String firstName, String lastName, int id, double gpa, int numSmallQuestions, int numLargeQuestions)
     {
         this.firstName = firstName;
@@ -24,6 +37,10 @@ public final class Student implements Comparable<Student>
         this.numSmallQuestions = numSmallQuestions;
         this.numLargeQuestions =numLargeQuestions;
     }
+
+    static public String[] getPriorities() {return priorities;}
+
+    static public void setCompareToPriority(String priority) {currentPriority = priority;}
 
     @Override
     public int compareTo(Student student)
