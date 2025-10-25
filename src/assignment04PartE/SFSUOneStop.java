bc9b3ac91825ade4ed0ade19d0e62e393de3af74
Robin Lane <rubeculabee@gmail.com>
2025-10-24 18:17:56 -0700

    Added comments and project headers

--- src/assignment04PartE/SFSUOneStop.java
+++ src/assignment04PartE/SFSUOneStop.java
@@ -6,10 +6,24 @@ package assignment04PartE;
  *
  */
 
+/*
+ * File: SFSUOneStop.java
+ * By:   Robin Lane
+ * Date: 10-24-25
+ * 
+ * Description: A simple class representing the One Stop Student Services Center.
+ *              it's only purpose is to correctly format and display each
+ *              priority queue of students.
+ */
+
 import java.util.PriorityQueue;
 
 public class SFSUOneStop 
 {
+    /**
+     * @param pq The priority queue to be displayed
+     * @param priority The name of the attribute we are sorting by
+     */
 	public static void display(PriorityQueue<Student> pq, String priority)
     {
         System.out.printf("Priority: %s%s\n", priority, priority=="realistic"? " (provided by supervisors)" : "");
--- src/assignment04PartE/Student.java
+++ src/assignment04PartE/Student.java
@@ -9,8 +9,20 @@ import java.util.Arrays;
  *
  */
 
+ /*
+ * File: Student.java
+ * By:   Robin Lane
+ * Date: 10-24-25
+ * 
+ * Description: Represents a single student at SFSU. The student should have a first 
+ *              and last name, a student id, a gpa, and numbers of small and big questions.
+ *              The student class also tracks a list of priorities (ways of comparing
+ *              the student with other students), as well as which priority is currently selected
+ */
+
 public final class Student implements Comparable<Student>
 {
+    // The students attributes are stored in an array, so that they can be iterated through.
     private Comparable[] attributes;
 
     static private int currentPriorityIndex;
@@ -28,11 +40,16 @@ public final class Student implements Comparable<Student>
 
     public Student (String firstName, String lastName, int id, double gpa, int numSmallQuestions, int numLargeQuestions)
     {
+        // The attribute at index 0 is the last letter of the students last name.
         attributes = new Comparable[] {lastName.charAt(lastName.length()-1), firstName, lastName, id, gpa, numSmallQuestions, numLargeQuestions, numSmallQuestions + numLargeQuestions};
     }
 
     static public String[] getPriorities() {return priorities;}
 
+    /**
+     * @param priority The attribute that the students should be compared with
+     * @throws InvalidParameterException If the given priority isn't a valid option
+     */
     static public void setCompareToPriority(String priority) throws InvalidParameterException
     {
         for(int i = 0; i < priorities.length; i++)
@@ -42,8 +59,6 @@ public final class Student implements Comparable<Student>
                 return;
             }
 
-
-        // If the requested priority isn't in the list, throw an exception
         throw new InvalidParameterException();
     }
 
@@ -53,14 +68,27 @@ public final class Student implements Comparable<Student>
         return compareAttribute(currentPriorityIndex, student);
     }
 
+    /**
+     * @param priorityIndex The priority we are using to compare
+     * @param student The other student that is being compared with this student
+     * @return The difference in the attribute of the two students
+     */
     private int compareAttribute(int priorityIndex, Student student)
     {
+        // Compare the given attribute
         int result = this.attributes[priorityIndex].compareTo(student.attributes[priorityIndex]);
 
+        // If they are the same
         if(result == 0)
             if(priorityIndex == 0)
+                //If the priority index is 0, pick a priority index based on the length of the last name
+                /* 3 letters becomes index 1 priority (first-name)
+                   4 letters becomes index 2 priority (last-name)
+                   5 letters becomes index 3 priority (student-id)
+                   Anything higher loops through those three options since we are doing mod 3 */
                 return compareAttribute((((String) attributes[2]).length() % 3) + 1, student);
             else
+                //Compare with the previous attribute
                 return compareAttribute(priorityIndex-1, student);
 
         return result;
@@ -69,6 +97,7 @@ public final class Student implements Comparable<Student>
     @Override
     public String toString()
     {
+        // We only want to print the attributes from first-name to number-of-big-questions
         return String.format("%-10s%-10s%-10d%-3.2f%9d%8d", Arrays.copyOfRange(attributes, 1, 7));
     }
 }
